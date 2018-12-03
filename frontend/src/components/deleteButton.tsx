b8bd25541c327de6c167c274aaf3fa40490309d0
jallier <justin.hallier+git@gmail.com>
2018-12-03 21:37:44 +1300

    Make the delete icon show on click
    This is changed from on hover to work better with mobile.

--- frontend/src/components/deleteButton.tsx
+++ frontend/src/components/deleteButton.tsx
@@ -3,16 +3,21 @@ import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 import styled from 'react-emotion';
 
+interface StyledIconProps {
+  show: boolean;
+}
+
 const StyledIconButton = styled(IconButton)`
-  display: none;
+  display: ${(props: StyledIconProps) => props.show ? 'block' : 'none'};
   margin-left: 5px;
 `;
 
 interface DeleteButtonProps {
   text?: string;
   color?: string;
-  onClick?: ((event: React.MouseEvent<HTMLElement>) => void) | undefined; 
   // This seems to be the type definition for the onClick prop
+  onClick?: ((event: React.MouseEvent<HTMLElement>) => void) | undefined;
+  show: boolean;
 }
 
 export class DeleteButton extends React.Component<DeleteButtonProps> {
@@ -23,7 +28,7 @@ export class DeleteButton extends React.Component<DeleteButtonProps> {
   render() {
     return (
       <div>
-        <StyledIconButton aria-label="delete"  onClick={this.props.onClick} >
+        <StyledIconButton aria-label="delete" onClick={this.props.onClick} show={this.props.show}>
           <Icon>delete</Icon>
         </StyledIconButton>
       </div>
